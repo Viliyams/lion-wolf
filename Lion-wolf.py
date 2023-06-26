c7edb52991ad2780bf55def190e430155fd7970b
viliyams <90315707+Viliyams@users.noreply.github.com>
2023-06-27 00:16:20 +0530

    Update Lion-wolf.py

--- Lion-wolf.py
+++ Lion-wolf.py
@@ -2,6 +2,10 @@ import itertools
 
 def brute_force(target, characters, max_length):
     for length in range(1, max_length + 1):
+        for attempt in itertools.product(characters, import itertools
+
+def brute_force(target, characters, max_length):
+    for length in range(1, max_length + 1):
         for attempt in itertools.product(characters, repeat=length):
             password = ''.join(attempt)
             if password == target:
@@ -14,4 +18,3 @@ maximum_length = 8
 
 result = brute_force(target_password, character_set, maximum_length)
 print("Found password:", result)
-
